251c685669b2441fbc2891508053ce8c128faba7
ZooSky <zoosky@gmail.com>
2019-12-07 13:31:39 +0100

    exercise1

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ extern crate globwalk;
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
-    for img in globwalk::glob("*.*").unwrap() {
+    println!("Hello, httpredirect server!");
+    for img in globwalk::glob("*.rs").unwrap() {
         if let Ok(file) = img {
-            println!("{:?}", file);
+            print!("{:?} - ", file.file_name());
             //s::remove_file(img.path()).unwrap();
             let attr = fs::metadata(file.path());
             // inspect attr ...
